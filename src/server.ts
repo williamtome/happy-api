a7b3d71f4f86a94b581bd0ca876f5ef074ad5aa5
William Tomé <william.tomenh@gmail.com>
2020-10-13 21:12:19 -0300

    primeira rota

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,17 @@
 import express from 'express';
 
+import './database/connection';
+
 const app = express()
+
+app.use(express.json())
+
+app.get('/users', (req, res) => {
+  return res.json({
+    'name': 'William',
+    'age': 32,
+    'sex': 'M'
+  })
+})
 
 app.listen(3333)
